6c587daa3cd9c80bcd9de9eb835d064fee20d0e9
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-07-04 11:52:19 +0200

    Refactor RestMainController /receive endpoint & add Exceptionhandler

--- src/main/java/com/greenfox/Controller/RestMainController.java
+++ src/main/java/com/greenfox/Controller/RestMainController.java
@@ -3,7 +3,9 @@ package com.greenfox.Controller;
 import com.greenfox.Modell.*;
 import com.greenfox.Service.MessageOperator;
 import com.greenfox.Service.MessageValidator;
+import groovy.lang.MissingFieldException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,19 +20,21 @@ public class RestMainController {
   @Autowired
   private MessageValidator messageValidator;
 
+  @ResponseStatus(code = HttpStatus.OK)
   @CrossOrigin("*")
   @PostMapping("/api/message/receive")
   public Status receiveMessage(@RequestBody MessageToBroadcast messageToBroadcast) {
-   if (messageOperator.messageAlreadyExists(messageToBroadcast)) {
-     return new MessageExistsStatus();
-   } else {
-     if (messageValidator.validateMessage(messageToBroadcast)) {
+   if (!messageOperator.messageAlreadyExists(messageToBroadcast) && messageValidator.validateMessage(messageToBroadcast)) {
        messageOperator.saveAndForwardMessage(messageToBroadcast);
        return new okStatus();
-     } else {
-       return new errorStatus("error", "Missing fields:");
-     }
-   }
+     } else
+       return new MessageExistsStatus();
+  }
+
+  @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+  @ExceptionHandler(MissingFieldException.class)
+  public Status missingFields() {
+    return new errorStatus("error", "Missing fields:");
   }
 
 }
